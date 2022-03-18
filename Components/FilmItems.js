8739412bf8fb680bef3f1bc75fabdbd5ffe60628
LoicPfeiffer <95574385+LoicPfeiffer@users.noreply.github.com>
2022-03-18 16:36:27 +0100

    Update FilmItems.js

--- Components/FilmItems.js
+++ Components/FilmItems.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { View, Text, Image, StyleSheet } from "react-native";
+import { Pressable } from "react-native";
+import { getImageFromApi } from "../API/TMDBApi";
 class FilmItem extends React.Component {
   render() {
-    const film = this.props.film;
+    const { film, displayDetailForFilm } = this.props;
+    /* const film = this.props.film;*/
     console.log(this.props.film.title);
     return (
       <View>
         <Text>{film.title}</Text>
-        <Image
-          style={styles.image}
-          source={{
-            uri: "https://reactnative.dev/img/tiny_logo.png",
-          }}
-        />
+        <Pressable onPress={() => displayDetailForFilm(film.id)}>
+          <Image
+            style={styles.image}
+            source={getImageFromApi(film.poster_path)}
+          />
+        </Pressable>
       </View>
     );
   }
